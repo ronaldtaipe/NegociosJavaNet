582598436dd5f5e7b5985305ed114d19f6f586b4
ronaldtaipe <get -version>
2012-09-22 09:49:31 -0500

    Version 2.1 - Proyecto WebService SOAP

---
+++ Negocios_JavaNet_2.1/WebStore/Marcas.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/webstore.Master" AutoEventWireup="true" CodeBehind="Marcas.aspx.cs" Inherits="WebStore.paginasWebs.Marcas" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+ <%--Copiar esta parte para que quede centrado--%>
+<div class="wrapper row2">
+ <div id="container" class="clear">
+    <!-- content body -->
+    <section id="content">
+     <%--Copiar esta parte para que quede centrado--%>
+
+
+
+
+
+      <%--Copiar esta parte para que quede centrado--%>
+  </section>
+    </div>
+</div>
+    <%--Copiar esta parte para que quede centrado--%>
+</asp:Content>
---
+++ Negocios_JavaNet_2.1/WebStore/RegistrarCliente.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using rnd = WebStore.validapersona;
+using System.Drawing;
+
+
+	
+namespace WebStore
+{
+    public partial class RegistrarCliente : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnRegistrar_Click(object sender, EventArgs e)
+        {
+            /*
+            rnd.PersonaServiceImplService wsRnd = new rnd.PersonaServiceImplService();
+            int valida = wsRnd.validaPersona(txtDni.Text);
+            if (valida == 1)
+            {
+                lblResultado.Text = "Registro Completo ... ";
+                lblResultado.ForeColor = System.Drawing.Color.Blue;
+                Response.AddHeader("REFRESH", "3;URL=Catalogo.aspx");                
+            }
+            else
+            {
+                lblResultado.ForeColor = System.Drawing.Color.Red;
+                lblResultado.Text = "Ustede No Puede Registrarse";
+            }*/
+            rnd.PersonaServiceImplService wsRnd = new rnd.PersonaServiceImplService();
+            String valida = wsRnd.validaPersonaInfo(txtDni.Text);
+            string csv = valida;
+            string[] parts = csv.Split(';');
+            List<string> list = new List<string>(parts);
+            String estado = list[0].ToString();
+
+            if (estado.Equals("1"))
+            {
+                txtNombres.Text = list[1].ToString();
+                txtApellidos.Text = list[2].ToString();
+                txtTelefono.Text = list[3].ToString();
+                txtDireccion.Text = list[4].ToString();
+                lblResultado.Text = "Registro Completo ... ";
+                lblResultado.ForeColor = System.Drawing.Color.Blue;
+                Response.AddHeader("REFRESH", "6;URL=Catalogo.aspx");
+            }
+            else
+            {
+                lblResultado.ForeColor = System.Drawing.Color.Red;
+                lblResultado.Text = "Ustede No Puede Registrarse";
+            }
+
+
+
+        }
+
+        protected void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("index.aspx");
+        }
+    }
+}
